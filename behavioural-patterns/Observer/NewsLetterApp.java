740a5fc6bc8ce5901f1aebc6da203f6b647af058
Suraj Mukhia <surajmukhia95@gmail.com>
2024-04-10 15:49:02 +0600

    Update NewsLetterApp.java

--- behavioural-patterns/Observer/NewsLetterApp.java
+++ behavioural-patterns/Observer/NewsLetterApp.java
@@ -51,7 +51,7 @@ class NewsLetterUser implements User{
         System.out.println("User: " + name);
         System.out.println("Reading the Kunsel News");
         for(Article article: news.getArticles()){
-            System.out.println(article);
+            System.out.println("\t" + article);
         }
     }
 
@@ -61,12 +61,12 @@ class NewsLetterUser implements User{
 interface NewsLetter{
     public void subscribe(User user);
     public void unSubscribe(User user);
-    public void notifySubscriber();
+    public void notifySubscriber();  
     public void publish(News news);
 }
 
 //Concrete Subject
-class Kunesel implements NewsLetter{
+class Kuensel implements NewsLetter{
     private HashMap<String, User> subscribers = new HashMap<>();
     private News news;
     
@@ -94,19 +94,19 @@ class Kunesel implements NewsLetter{
 public class NewsLetterApp {
     public static void main(String[] args){
         //Lets build the Kunsel News First
-        News kuneslNews = new News();
-        kuneslNews.addArticle(new Article("Rising Temperature", "The global warming is causing the rise in the temperature"));
-        kuneslNews.addArticle(new Article("Forest Fire", "The forest fire broke out in the capital"));
+        News kuenselNews = new News();
+        kuenselNews.addArticle(new Article("Rising Temperature", "The global warming is causing the rise in the temperature"));
+        kuenselNews.addArticle(new Article("Forest Fire", "The forest fire broke out in the capital"));
 
         //Real Subject
-        Kunesel kunesel = new Kunesel();
-        kunesel.subscribe(new NewsLetterUser("Sonam", "sonam123"));
-        kunesel.subscribe(new NewsLetterUser("John", "john123"));
+        Kuensel kuensel = new Kuensel();
+        kuensel.subscribe(new NewsLetterUser("Sonam", "sonam123"));
+        kuensel.subscribe(new NewsLetterUser("John", "john123"));
 
         //Lets publish the news
-        kunesel.publish(kuneslNews); //Should see all the user in the console with the news
+        kuensel.publish(kuenselNews); //Should see all the user in the console with the news
 
-        kunesel.unSubscribe(new NewsLetterUser("John", "john123"));
-        kunesel.publish(kuneslNews); // Publish same news, only Sonam should see the news
+        kuensel.unSubscribe(new NewsLetterUser("John", "john123"));
+        kuensel.publish(kuenselNews); // Publish same news, only Sonam should see the news
     }
 }
